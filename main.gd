9e22b1e3c5846a154345a7570ee435cfd039abf1
WOCae <t-satoIZ@outlook.jp>
2024-02-23 19:24:17 +0900

    Update main.gd

--- main.gd
+++ main.gd
@@ -1,29 +1,46 @@
 extends Node2D
+#時間の表示
 var elapsed_time:float
 var elapsed_time_ph:float
 
-@onready var _camera = $Camera2D # カメラ.
-var _tShake: float = 0 # 画面揺らすタイマー.
-const TIMER_SHAKE = 0.5 # 0.5秒の間揺らす
+@onready var Camera = $Camera2D # カメラ.
+#@export var speed = 5
+var moveCam: float = 0 
+const moveTime = 0.5 
+
 func _process(delta):
 	elapsed_time += delta	
 	$elapsed_time.text = "経過時間"+"%7.1f" % elapsed_time	
 
+
+	var velocity = Vector2.ZERO
+
+	if Input.is_action_pressed("ui_up"):
+		Camera.position.y -= 100*delta # moveup
+
+	if Input.is_action_pressed("ui_down"):
+		Camera.position.y += 100*delta # movedown
+
+	if Input.is_action_pressed("ui_left"):
+		Camera.position.x -= 100*delta # moveleft
+
+	if Input.is_action_pressed("ui_right"):
+		Camera.position.x += 100*delta # moveright
+
 	if Input.is_action_pressed(("ui_select"))|| Input.is_action_pressed(("start")):
 	# 画面揺らす
-		_camera.offset = Vector2.ZERO
-		_tShake -= delta
-		if _tShake < 0:
-			var rate = _tShake / TIMER_SHAKE
+		Camera.offset = Vector2.ZERO
+		moveCam -= delta
+		if moveCam < 0:
+			var rate = moveCam / moveTime
 			var vx = 64 * rate
 			var vy = 16 * rate
-			_camera.offset.x = randi_range(-vx, vx)
-			_camera.offset.y = randi_range(-vy, vy)
-			# 画面揺れ中はリスタートできなくする.
+			Camera.offset.x = randi_range(-vx, vx)
+			Camera.offset.y = randi_range(-vy, vy)
 			return
 
-	_camera.offset = Vector2.ZERO
-	_tShake = 0
+	Camera.offset = Vector2.ZERO
+	moveCam = 0
 
 func _physics_process(delta):
 	elapsed_time_ph += delta	
